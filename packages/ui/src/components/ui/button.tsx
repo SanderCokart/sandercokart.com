97437a49eabaa1fa4078093413b21e24612006f0
Sander Cokart <cokart32@gmail.com>
2024-06-04 01:30:59 +0200

    Add form components and update button component
    Implemented new textarea, label, and input components that will be used for form handling. The button component has been updated to set a type as 'button' by default. The 'react-hook-form' and '@hookform/resolvers' have been added as dependencies for form state management. Also, these components have been exposed in the package.json exports.

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -40,10 +40,10 @@ export interface ButtonProps
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, type = 'button', asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
 
-    return <Comp ref={ref} className={cn(buttonVariants({ variant, size, className }))} {...props} />;
+    return <Comp ref={ref} className={cn(buttonVariants({ variant, size, className }))} type={type} {...props} />;
   },
 );
 Button.displayName = 'Button';
