4a8dce6957237a971cdfb0ee8838d5745d0e4fea
Sander Cokart <cokart32@gmail.com>
2024-06-04 22:49:37 +0200

    CH-2: Replace Zod with Yup for form validation and add multilingual support
    The Zod library was replaced by Yup for form validation to provide more user-friendly error messages. Added 'yup-locales' package to support internationalization of these messages. The implementation is demonstrated in the 'submit-contact-form.ts' file where form validation errors are localized based on the user's locale. Removed 'prettier-plugin-tailwindcss' from the project.

--- apps/codehouse.sandercokart.com/src/app/[locale]/sections/actions/submit-contact-form.ts
+++ apps/codehouse.sandercokart.com/src/app/[locale]/sections/actions/submit-contact-form.ts
@@ -1,27 +1,42 @@
 'use server';
 
-import { z } from 'zod';
+import { getLocale } from 'next-intl/server';
+import * as Yup from 'yup';
+import { setLocale, ValidationError } from 'yup';
+import translations from 'yup-locales';
 
-//todo i18n
-const schema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  subject: z.string(),
-  message: z.string(),
+const schema = Yup.object({
+  name: Yup.string().required(),
+  email: Yup.string().email().required(),
+  subject: Yup.string().required(),
+  message: Yup.string().required(),
 });
 
+type Locales = 'en' | 'nl';
+
 export async function submitContactForm(formData: FormData) {
-  const result = schema.safeParse(formData);
-  if (!result.success) {
-    return result.error.formErrors.fieldErrors;
+  const locale = (await getLocale()) as Locales;
+  setLocale(translations[locale]);
+
+  let mail;
+
+  try {
+    mail = schema.validate(formData);
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      return error.errors;
+    }
   }
 
-  const response = await fetch(`${process.env.API_URL}/contact`, {
-    method: 'POST',
-    body: JSON.stringify(result.data),
-  });
+  try {
+    const response = await fetch(`${process.env.API_URL}/contact`, {
+      method: 'POST',
+      body: JSON.stringify(mail),
+    });
+    const json = await response.json();
 
-  const json = await response.json();
-
-  return json;
+    return json;
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
 }
