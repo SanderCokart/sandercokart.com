d493a22be6beb7ef19c270c27cfdc502c14d276a
Sander Cokart <cokart32@gmail.com>
2024-06-08 21:26:15 +0200

    Init admin.sandercokart.com

---
+++ apps/admin.sandercokart.com/tailwind.config.mjs
@@ -0,0 +1,21 @@
+import codeHouse from '@repo/ui/tailwind.config';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "../../packages/**/src/**/*.{js,ts,jsx,tsx,mdx}"],
+    presets: [codeHouse],
+    theme:{
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-geist-sans)'],
+                mono: ['var(--font-geist-mono)'],
+                digital: ["var(--font-digital)"],
+            },
+            colors:{
+                react: "hsl(193 95% 68%)",
+                laravel: "hsl(3 100% 56%)",
+                vue: "hsl(153 47% 49%)",
+            }
+        }
+    }
+};
