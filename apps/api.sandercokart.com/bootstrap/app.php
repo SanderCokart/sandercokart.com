3a93e4cf94aca54f0df86f36ada5c83940e0e72c
Sander Cokart <cokart32@gmail.com>
2024-06-08 19:08:54 +0200

    Add CORS configuration and update routing setup
    The update modifies the application's routing setup by disabling the default web routes and overriding default route prefixes and middleware. Also, a new configuration file for Cross-Origin Resource Sharing (CORS) has been added, setting up allowance for all origin communication end-points.

--- apps/api.sandercokart.com/bootstrap/app.php
+++ apps/api.sandercokart.com/bootstrap/app.php
@@ -6,10 +6,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        using: function() {
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
+        },
+        //web: __DIR__.'/../routes/web.php',
+        //api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
