abed4fa99bfdf4af1ed73ff19a8ac3d6df8e07c3
Sander Cokart <cokart32@gmail.com>
2024-06-09 14:52:34 +0200

    Simplify ThemeProvider and modify dev script
    Simplified the ThemeProvider in global-providers.tsx by removing unnecessary attributes. Separated the 'dev' and 'turbo' scripts in package.json to enhance the clarity and organization of the script commands.

--- apps/codehouse.sandercokart.com/src/components/global-providers.tsx
+++ apps/codehouse.sandercokart.com/src/components/global-providers.tsx
@@ -3,9 +3,5 @@ import type { ReactNode } from 'react';
 import { ThemeProvider } from './theme-provider';
 
 export function GlobalProviders({ children }: { children: ReactNode }) {
-  return (
-    <ThemeProvider disableTransitionOnChange enableSystem attribute="class" defaultTheme="system">
-      {children}
-    </ThemeProvider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
