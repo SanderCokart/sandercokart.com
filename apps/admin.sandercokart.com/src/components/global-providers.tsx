bc5af5e0ba87b1c9b22eae992de15dbb74de9ecf
Sander Cokart <cokart32@gmail.com>
2024-06-15 20:16:39 +0200

    Update article type and add SWR configuration
    The type of articles imported in `_auth.articles.tsx` is updated from 'Article' to 'ArticleIndex'. A SWRConfig wrapper is also added in 'global-providers.tsx' to handle fetching of data, which contributes to standardising and simplifying data fetching in the application.

--- apps/admin.sandercokart.com/src/components/global-providers.tsx
+++ apps/admin.sandercokart.com/src/components/global-providers.tsx
@@ -1,15 +1,23 @@
+import { SWRConfig } from 'swr';
+
 import type { ReactNode } from 'react';
 
 import { ThemeProvider } from '@/components/theme-provider.tsx';
 
+import { api } from '@/lib/api.ts';
 import { AuthProvider } from '@/lib/auth.tsx';
 
 export function GlobalProviders({ children }: { children: ReactNode }) {
   return (
-    <AuthProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="'vite-ui-theme">
-        {children}
-      </ThemeProvider>
-    </AuthProvider>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => api.get(url).then(res => res.data),
+      }}>
+      <AuthProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="'vite-ui-theme">
+          {children}
+        </ThemeProvider>
+      </AuthProvider>
+    </SWRConfig>
   );
 }
--- apps/admin.sandercokart.com/src/routes/_auth.articles.tsx
+++ apps/admin.sandercokart.com/src/routes/_auth.articles.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import type { Article } from '@/types/models.ts';
+import type { ArticleIndex } from '@/types/models.ts';
 
 import { ArticlesTable } from '@/components/articles-table.tsx';
 
@@ -9,7 +9,7 @@ import { api } from '@/lib/api.ts';
 export const Route = createFileRoute('/_auth/articles')({
   beforeLoad: () => ({
     fetchArticles: async ({ signal }: { signal: AbortSignal }) => {
-      const { data: articles } = await api.get<Article[]>('/articles', { signal });
+      const { data: articles } = await api.get<ArticleIndex[]>('/articles', { signal });
 
       return articles;
     },
