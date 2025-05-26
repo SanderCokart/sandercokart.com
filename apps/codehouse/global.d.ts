2363083f166e7c624cbb115160150f91afa8b906
Sander Cokart <cokart32@gmail.com>
2025-05-26 22:39:54 +0200

    "Update pnpm-lock.yaml: bump dependencies and add '@lingual/i18n-check' support"

--- apps/codehouse/global.d.ts
+++ apps/codehouse/global.d.ts
@@ -1,3 +1,6 @@
+import { formats } from '@/src/i18n/request';
+import { routing } from '@/src/i18n/routing';
+
 type GlobalMessages = typeof import('./messages/en.json');
 type ZodMessages = typeof import('./messages/zod/en.json');
 type Messages = GlobalMessages & ZodMessages;
@@ -5,4 +8,12 @@ type Messages = GlobalMessages & ZodMessages;
 declare global {
   // Use type safe message keys with `next-intl`
   interface IntlMessages extends Messages {}
+}
+
+declare module 'next-intl' {
+  interface AppConfig {
+    Locale: (typeof routing.locales)[number];
+    Messages: typeof Messages;
+    Formats: typeof Formats;
+  }
 }
