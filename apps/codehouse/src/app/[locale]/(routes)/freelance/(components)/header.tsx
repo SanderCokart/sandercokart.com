f7d3bdfa2ca548aa87ed06c2fd49fb2c8abf5694
Sander Cokart <cokart32@gmail.com>
2026-03-13 13:16:08 +0100

    refactor(codehouse): use NamedLogo href/Component in freelance header

--- apps/codehouse/src/app/[locale]/(routes)/freelance/(components)/header.tsx
+++ apps/codehouse/src/app/[locale]/(routes)/freelance/(components)/header.tsx
@@ -1,21 +1,23 @@
 import { Header as BrandHeader } from '@repo/ui/components/header';
 import { NamedLogo } from '@repo/ui/components/header/named-logo';
 
+
+
 import { LocaleSwitcher } from '@/src/components/locale-switcher';
 import { Link } from '@/src/i18n/navigation';
 
 import { NavigationHeader } from './navigation-header';
+
+
+
+
 
 export function Header() {
   return (
     <BrandHeader
       localeSwitcher={<LocaleSwitcher />}
       navigation={<NavigationHeader />}
-      namedLogo={
-        <Link href="/">
-          <NamedLogo name="Sander's Codehouse" slogan="Let's code..." />
-        </Link>
-      }
+      namedLogo={<NamedLogo href="/" Component={Link} name="Sander's Codehouse" slogan="Let's code..." />}
     />
   );
 }
