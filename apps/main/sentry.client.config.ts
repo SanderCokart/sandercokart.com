3a9c1dede9eaf97c7aeabb161950df867f6e8790
Sander Cokart <cokart32@gmail.com>
2026-03-16 21:07:05 +0100

    FORMAT | Clean up Sentry client config

--- apps/main/sentry.client.config.ts
+++ apps/main/sentry.client.config.ts
@@ -1,17 +1,10 @@
-;
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
 
-
-
 import { env } from '@/env';
-
-
-
-
 
 Sentry.init({
   enabled: env.NEXT_PUBLIC_SENTRY_ENABLED,
