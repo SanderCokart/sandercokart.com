821d159a183525abd5614a3f48d95b3e93eb428e
Sander Cokart <cokart32@gmail.com>
2026-03-27 00:38:49 +0100

    REFACTOR | UI turbo generator and package metadata

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
--- packages/ui/src/components/header/named-logo.tsx
+++ packages/ui/src/components/header/named-logo.tsx
@@ -1,5 +1,3 @@
-import type { ReactNode } from 'react';
-
 interface NamedLogoProps {
   name: string;
   slogan: string;
