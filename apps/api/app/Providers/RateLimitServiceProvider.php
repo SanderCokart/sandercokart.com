2492012fd29b3f93121738a5989827d91eeeb157
Sander Cokart <cokart32@gmail.com>
2026-03-27 01:43:57 +0100

    #9 | REFACTOR | Extract API rate limiter registration to dedicated provider

---
+++ apps/api/app/Providers/RateLimitServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+
+class RateLimitServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('contact', function (Request $request) {
+            return Limit::perHour(2)->by($request->ip());
+        });
+    }
+}
